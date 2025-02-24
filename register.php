aff49b37b9e2fd57f1b55b991ecf1d277d0ffecb
sophia5656 <slee16085@gmail.com>
2025-02-24 09:01:59 +0100

    Update register.php

--- register.php
+++ register.php
@@ -20,11 +20,11 @@
         /* Container for the registration form */
         .registration-container {
             background: white;
-            padding: 30px;
+            padding: 50px;
             border-radius: 10px;
             box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
             width: 100%;
-            max-width: 400px;
+            max-width: 600px;
             text-align: center;
         }
 
